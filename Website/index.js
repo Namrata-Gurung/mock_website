759053e486a3bcca66395f95116ed7b1fd034188
Namrata-Gurung <125033360+Namrata-Gurung@users.noreply.github.com>
2025-01-08 04:40:36 +0000

    Update to hamburger menu and added different styles for different devices

--- Website/index.js
+++ Website/index.js
@@ -1,4 +1,32 @@
 
-function menuFunction(x) {
+//NAVIGATION----------------------------
+
+//toggle the hamburger menu when clicked
+function openMenu(x) {
   x.classList.toggle("change")
+  document.getElementById("navigation-overlay").style.display ="flex";
 }
+function closeMenu(x){
+    x.classList.toggle("change")
+    document.getElementsById("overlay-links").style.display = "none";
+}
+
+//HEADER--------------------------------
+
+
+//QUOTES--------------------------------
+
+
+//OUR-STORY-----------------------------
+
+
+//MENU----------------------------------
+
+
+//REVIEWS-------------------------------
+
+
+//RESERVATIONS--------------------------
+
+
+//INFORMATION---------------------------
